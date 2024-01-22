3763dfc005e91c8f55e72baeb5440a41f8cc859b
Austin Ziegler <austin@zieglers.ca>
2024-01-22 00:30:24 -0500

    Adding tests and significant update
    - Tests have been added on the "golden file" model using the goldenfile
      crate.
    
      - This makes comparison very easy and predictable.
    
      - It also allows us to quickly verify that, in the case of multiple
        file collection and output, we have meaningful output that shows the
        files are sorted.
    
      - Six golden files (one for each alignment mode) are used for three
        integration test cases:
    
        - single file (always the current `Cargo.lock`);
    
        - duplicate file (always the current `Cargo.lock` specified twice);
          these reuse the single file golden files because there should be
          exactly *no* difference because of our use of the set;
    
        - multiple file (fixed files in the `Cargo.lock` version 3 format,
          pulled from `rust-lang/rust/Cargo.lock`  and
          `rust-lang/rust/bootstrap/Cargo.lock`. These are
          `multi_lockfile_one` and `multi_lockfile_two`, respectively.
          Hopefully this naming will prevent unnecessary Dependabot updates.
    
      - The golden files for the single file will have to be updated on each
        release where dependencies are updated.
    
      - The golden files for multiple files should be updated when there is
        a difference in the `Cargo.lock` version. We do not need to track
        these files.
    
    - Split the code into two files (lib and main).
    
      - This rearrangement required that a couple of functions have their
        return type modified so that we can meaningfully use `function()?`
        and defer exit from the library and only have it happen in the
        binary.
    
      - Added `format_cargo_crates(Vec<Package>, AlignmentMode) -> String`
        to make it easier to use golden file testing. The two downsides are
        that no output will happen until the output is complete and that
        memory use will increase as we are collecting the output in a string
        before output.
    
    - Added some smarts to the binary portion so that if I specify
      `cargo2port . bootstrap`, it will recognize that both `.` and
      `bootstrap` are directories and check for `./Cargo.lock` and
      `bootstrap/Cargo.lock`. This means you only have to specify the
      *directories* from the command-line, not the full path to Cargo.lock.

---
+++ tests/multi_file_test.rs
@@ -0,0 +1,44 @@
+use std::io::Write;
+
+use cargo2port::{format_cargo_crates, read_packages_from_lockfiles, AlignmentMode};
+use goldenfile::Mint;
+
+fn lockfiles() -> Vec<String> {
+    return vec![
+        "tests/support/multi_lockfile_one".to_string(),
+        "tests/support/multi_lockfile_two".to_string(),
+    ];
+}
+
+#[test]
+fn test_multi_file_normal_mode() {
+    let mut mint = Mint::new("tests/support");
+    let mut file = mint.new_goldenfile("multi_file_normal").unwrap();
+
+    let packages = read_packages_from_lockfiles(&lockfiles()).unwrap();
+    let output = format_cargo_crates(packages, AlignmentMode::Normal);
+
+    writeln!(file, "{}", output).unwrap();
+}
+
+#[test]
+fn test_multi_file_maxlen_mode() {
+    let mut mint = Mint::new("tests/support");
+    let mut file = mint.new_goldenfile("multi_file_maxlen").unwrap();
+
+    let packages = read_packages_from_lockfiles(&lockfiles()).unwrap();
+    let output = format_cargo_crates(packages, AlignmentMode::Maxlen);
+
+    writeln!(file, "{}", output).unwrap();
+}
+
+#[test]
+fn test_multi_file_multiline_mode() {
+    let mut mint = Mint::new("tests/support");
+    let mut file = mint.new_goldenfile("multi_file_multiline").unwrap();
+
+    let packages = read_packages_from_lockfiles(&lockfiles()).unwrap();
+    let output = format_cargo_crates(packages, AlignmentMode::Multiline);
+
+    writeln!(file, "{}", output).unwrap();
+}
---
+++ tests/one_file_test.rs
@@ -0,0 +1,41 @@
+use std::io::Write;
+
+use cargo2port::{format_cargo_crates, read_packages_from_lockfiles, AlignmentMode};
+use goldenfile::Mint;
+
+fn lockfiles() -> Vec<String> {
+    return vec!["Cargo.lock".to_string()];
+}
+
+#[test]
+fn test_one_file_normal_mode() {
+    let mut mint = Mint::new("tests/support");
+    let mut file = mint.new_goldenfile("one_file_normal").unwrap();
+
+    let packages = read_packages_from_lockfiles(&lockfiles()).unwrap();
+    let output = format_cargo_crates(packages, AlignmentMode::Normal);
+
+    writeln!(file, "{}", output).unwrap();
+}
+
+#[test]
+fn test_one_file_maxlen_mode() {
+    let mut mint = Mint::new("tests/support");
+    let mut file = mint.new_goldenfile("one_file_maxlen").unwrap();
+
+    let packages = read_packages_from_lockfiles(&lockfiles()).unwrap();
+    let output = format_cargo_crates(packages, AlignmentMode::Maxlen);
+
+    writeln!(file, "{}", output).unwrap();
+}
+
+#[test]
+fn test_one_file_multiline_mode() {
+    let mut mint = Mint::new("tests/support");
+    let mut file = mint.new_goldenfile("one_file_multiline").unwrap();
+
+    let packages = read_packages_from_lockfiles(&lockfiles()).unwrap();
+    let output = format_cargo_crates(packages, AlignmentMode::Multiline);
+
+    writeln!(file, "{}", output).unwrap();
+}
