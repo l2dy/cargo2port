f4126597181830b494fcf21b83aa8acdbc8ebbbf
Austin Ziegler <austin@zieglers.ca>
2024-01-22 00:30:24 -0500

    Adding tests and significant update
    - Tests have been added on the "golden file" model using the goldenfile
      crate.
    
      - This makes comparison very easy and predictable.
    
      - It also allows us to quickly verify that, in the case of multiple
        file collection and output, we have meaningful output that shows the
        files are sorted.
    
      - Six golden files (one for each alignment mode) are used for three
        integration test cases:
    
        - single file (always the current `Cargo.lock`);
    
        - duplicate file (always the current `Cargo.lock` specified twice);
          these reuse the single file golden files because there should be
          exactly *no* difference because of our use of the set;
    
        - multiple file (fixed files in the `Cargo.lock` version 3 format,
          pulled from `rust-lang/rust/Cargo.lock`  and
          `rust-lang/rust/bootstrap/Cargo.lock`. These are
          `multi_lockfile_one` and `multi_lockfile_two`, respectively.
          Hopefully this naming will prevent unnecessary Dependabot updates.
    
      - The golden files for the single file will have to be updated on each
        release where dependencies are updated.
    
      - The golden files for multiple files should be updated when there is
        a difference in the `Cargo.lock` version. We do not need to track
        these files.
    
    - Split the code into two files (lib and main).
    
      - This rearrangement required that a couple of functions have their
        return type modified so that we can meaningfully use `function()?`
        and defer exit from the library and only have it happen in the
        binary.
    
      - Added `format_cargo_crates(Vec<Package>, AlignmentMode) -> String`
        to make it easier to use golden file testing. The two downsides are
        that no output will happen until the output is complete and that
        memory use will increase as we are collecting the output in a string
        before output.
    
    - Added some smarts to the binary portion so that if I specify
      `cargo2port . bootstrap`, it will recognize that both `.` and
      `bootstrap` are directories and check for `./Cargo.lock` and
      `bootstrap/Cargo.lock`. This means you only have to specify the
      *directories* from the command-line, not the full path to Cargo.lock.

---
+++ src/lib.rs
@@ -0,0 +1,109 @@
+use std::collections::BTreeSet;
+
+use cargo_lock::{Lockfile, Package};
+
+#[derive(PartialEq)]
+pub enum AlignmentMode {
+    Normal,
+    Maxlen,
+    Multiline,
+}
+
+pub fn read_packages_from_lockfiles(
+    files: &Vec<String>,
+) -> Result<Vec<Package>, cargo_lock::Error> {
+    let lockfiles = read_lockfiles(&files)?;
+    let packageset = create_packageset(&lockfiles);
+    let mut packages = Vec::new();
+
+    for package in packageset {
+        packages.push(package.clone())
+    }
+
+    packages.sort();
+
+    return Ok(packages);
+}
+
+pub fn format_cargo_crates(packages: Vec<Package>, mode: AlignmentMode) -> String {
+    let mut output = String::new();
+
+    let mut name_min_width = 0;
+    let mut version_min_width = 0;
+
+    if mode == AlignmentMode::Maxlen {
+        for package in &packages {
+            let name_len = package.name.as_str().len();
+            if name_len > name_min_width {
+                name_min_width = name_len;
+            }
+
+            let version_len = package.version.to_string().len();
+            if version_len > version_min_width {
+                version_min_width = version_len;
+            }
+        }
+    }
+
+    output.push_str("cargo crates");
+
+    for package in packages {
+        if let Some(checksum) = &package.checksum {
+            output.push_str(" \\\n");
+
+            let line = match mode {
+                AlignmentMode::Maxlen => format!(
+                    "    {:<name_width$}  {:<version_width$}  {}",
+                    package.name,
+                    package.version,
+                    checksum,
+                    name_width = name_min_width,
+                    version_width = version_min_width
+                ),
+                AlignmentMode::Multiline => format!(
+                    "    {} \\\n    {} \\\n    {}",
+                    package.name, package.version, checksum
+                ),
+                AlignmentMode::Normal => format!(
+                    "    {:<name_width$}  {:>version_width$}  {}",
+                    package.name,
+                    package.version,
+                    checksum,
+                    name_width = 28,
+                    version_width = 8
+                ),
+            };
+
+            output.push_str(&line);
+        }
+    }
+
+    return output;
+}
+
+fn read_lockfiles(names: &Vec<String>) -> Result<Vec<Lockfile>, cargo_lock::Error> {
+    let mut lockfiles: Vec<Lockfile> = vec![];
+
+    for name in names {
+        let lockfile = Lockfile::load(name)?;
+        lockfiles.push(lockfile);
+    }
+
+    return Ok(lockfiles);
+}
+
+fn create_packageset(lockfiles: &Vec<Lockfile>) -> BTreeSet<&Package> {
+    let mut packageset: BTreeSet<&Package> = BTreeSet::new();
+
+    for lockfile in lockfiles {
+        for package in &lockfile.packages {
+            if package.checksum.is_none() {
+                continue;
+            }
+
+            packageset.insert(package);
+        }
+    }
+
+    return packageset;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,8 @@
-use std::collections::BTreeSet;
 use std::env;
 use std::path::Path;
 use std::process;
 
-use cargo_lock::Lockfile;
-use cargo_lock::Package;
-
-#[derive(PartialEq)]
-enum AlignmentMode {
-    Normal,
-    Maxlen,
-    Multiline,
-}
-
-fn read_lockfiles(names: &Vec<String>) -> Vec<Lockfile> {
-    let mut lockfiles: Vec<Lockfile> = vec![];
-
-    for name in names {
-        match Lockfile::load(name) {
-            Ok(lockfile) => lockfiles.push(lockfile),
-            Err(e) => {
-                eprintln!("Error: {}", e);
-                print_usage(1);
-            }
-        }
-    }
-
-    return lockfiles;
-}
-
-fn create_packageset(lockfiles: &Vec<Lockfile>) -> BTreeSet<&Package> {
-    let mut packageset: BTreeSet<&Package> = BTreeSet::new();
-
-    for lockfile in lockfiles {
-        for package in &lockfile.packages {
-            if package.checksum.is_none() {
-                continue;
-            }
-
-            packageset.insert(package);
-        }
-    }
-
-    return packageset;
-}
-
-fn read_packages_from_lockfiles(files: &Vec<String>) -> Vec<Package> {
-    let lockfiles = read_lockfiles(&files);
-    let packageset = create_packageset(&lockfiles);
-    let mut packages = Vec::new();
-
-    for package in packageset {
-        packages.push(package.clone())
-    }
-
-    packages.sort();
-
-    return packages;
-}
+use cargo2port::{format_cargo_crates, read_packages_from_lockfiles, AlignmentMode};
 
 fn main() {
     let mut mode = AlignmentMode::Normal;
@@ -71,20 +16,10 @@ fn main() {
             "-h" => print_usage(0),
             "--align=maxlen" => mode = AlignmentMode::Maxlen,
             "--align=multiline" => mode = AlignmentMode::Multiline,
-            _ => {
-                let name = Path::new(&arg[..]);
-                match name.try_exists() {
-                    Ok(true) => files.push(arg),
-                    Ok(false) => {
-                        eprintln!("Error: cannot find file {arg}");
-                        process::exit(1);
-                    }
-                    Err(e) => {
-                        eprintln!("Error: {}", e);
-                        process::exit(1);
-                    }
-                }
-            }
+            _ => match check_path(&arg[..]) {
+                Some(path) => files.push(path),
+                None => process::exit(1),
+            },
         }
     }
 
@@ -92,60 +27,51 @@ fn main() {
         files.push("Cargo.lock".to_string())
     }
 
-    let packages = read_packages_from_lockfiles(&files);
-
-    if packages.len() == 0 {
-        eprintln!("No packages with checksums found.");
-        process::exit(0);
-    }
-
-    let mut name_min_width = 0;
-    let mut version_min_width = 0;
-
-    if mode == AlignmentMode::Maxlen {
-        for package in &packages {
-            let name_len = package.name.as_str().len();
-            if name_len > name_min_width {
-                name_min_width = name_len;
+    match read_packages_from_lockfiles(&files) {
+        Ok(packages) => {
+            if packages.len() == 0 {
+                eprintln!("No packages with checksums found.");
+                process::exit(0);
             }
 
-            let version_len = package.version.to_string().len();
-            if version_len > version_min_width {
-                version_min_width = version_len;
-            }
+            println!("{}", format_cargo_crates(packages, mode));
+        }
+        Err(error) => {
+            eprintln!("{}", error);
+            process::exit(1)
         }
     }
+}
 
-    print!("cargo.crates");
-    for package in &packages {
-        if let Some(checksum) = &package.checksum {
-            println!(" \\");
+fn check_path(arg: &str) -> Option<String> {
+    let path = Path::new(&arg);
 
-            match mode {
-                AlignmentMode::Maxlen => print!(
-                    "    {:<name_width$}  {:<version_width$}  {}",
-                    package.name,
-                    package.version,
-                    checksum,
-                    name_width = name_min_width,
-                    version_width = version_min_width
-                ),
-                AlignmentMode::Multiline => print!(
-                    "    {} \\\n    {} \\\n    {}",
-                    package.name, package.version, checksum
-                ),
-                AlignmentMode::Normal => print!(
-                    "    {:<name_width$}  {:>version_width$}  {}",
-                    package.name,
-                    package.version,
-                    checksum,
-                    name_width = 28,
-                    version_width = 8
-                ),
+    match path.try_exists() {
+        Ok(true) => {
+            if path.is_file() {
+                match path.to_str() {
+                    Some(path_str) => Some(path_str.to_string()),
+                    None => process::exit(1),
+                }
+            } else {
+                match path.join("Cargo.lock").to_str() {
+                    Some(file_path) => return check_path(file_path),
+                    None => {
+                        eprintln!("Error: failure appending Cargo.lock to {arg}");
+                        process::exit(1);
+                    }
+                }
             }
         }
+        Ok(false) => {
+            eprintln!("Error: cannot find file {arg}");
+            process::exit(1);
+        }
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            process::exit(1);
+        }
     }
-    println!();
 }
 
 fn print_usage(code: i32) -> () {
