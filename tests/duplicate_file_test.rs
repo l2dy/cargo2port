c93cd783577ee5e7b0505133c038772c89ff1019
Austin Ziegler <austin@zieglers.ca>
2024-08-18 16:31:28 -0400

    refactor: Document library use of cargo2port
    When src/lib.rs was added, cargo2port was made available for library
    use, but the functions were still organized in such a way as to reflect
    the cargo2port command line.
    
    This refactors the library code to make the functions more usable
    outside of the cargo2port command line context.
    
    The command-line file-to-package resolution has been moved into the
    binary crate, which required a workaround in the test suite to produce a
    `Vec<Package>` instead of `Vec<String>`.

--- tests/duplicate_file_test.rs
+++ tests/duplicate_file_test.rs
@@ -1,10 +1,17 @@
 use std::io::Write;
 
-use cargo2port::{format_cargo_crates, read_packages_from_lockfiles, AlignmentMode};
+use cargo2port::{
+    format_cargo_crates, lockfile_from_path, resolve_lockfile_packages, AlignmentMode,
+};
+use cargo_lock::Package;
 use goldenfile::Mint;
 
-fn lockfiles() -> Vec<String> {
-    vec!["Cargo.lock".to_string(), "Cargo.lock".to_string()]
+fn lockfiles() -> Vec<Package> {
+    let files = vec!["Cargo.lock".to_string(), "Cargo.lock".to_string()]
+        .into_iter()
+        .map(|f| lockfile_from_path(&f).unwrap())
+        .collect();
+    resolve_lockfile_packages(&files).unwrap()
 }
 
 #[test]
@@ -12,7 +19,7 @@ fn test_duplicate_file_normal_mode() {
     let mut mint = Mint::new("tests/support");
     let mut file = mint.new_goldenfile("one_file_normal").unwrap();
 
-    let packages = read_packages_from_lockfiles(&lockfiles()).unwrap();
+    let packages = lockfiles();
     let output = format_cargo_crates(packages, AlignmentMode::Normal);
 
     writeln!(file, "{}", output).unwrap();
@@ -23,7 +30,7 @@ fn test_duplicate_file_maxlen_mode() {
     let mut mint = Mint::new("tests/support");
     let mut file = mint.new_goldenfile("one_file_maxlen").unwrap();
 
-    let packages = read_packages_from_lockfiles(&lockfiles()).unwrap();
+    let packages = lockfiles();
     let output = format_cargo_crates(packages, AlignmentMode::Maxlen);
 
     writeln!(file, "{}", output).unwrap();
@@ -34,7 +41,7 @@ fn test_duplicate_file_multiline_mode() {
     let mut mint = Mint::new("tests/support");
     let mut file = mint.new_goldenfile("one_file_multiline").unwrap();
 
-    let packages = read_packages_from_lockfiles(&lockfiles()).unwrap();
+    let packages = lockfiles();
     let output = format_cargo_crates(packages, AlignmentMode::Multiline);
 
     writeln!(file, "{}", output).unwrap();
@@ -45,7 +52,7 @@ fn test_duplicate_file_justify_mode() {
     let mut mint = Mint::new("tests/support");
     let mut file = mint.new_goldenfile("one_file_justify").unwrap();
 
-    let packages = read_packages_from_lockfiles(&lockfiles()).unwrap();
+    let packages = lockfiles();
     let output = format_cargo_crates(packages, AlignmentMode::Justify);
 
     writeln!(file, "{}", output).unwrap();
